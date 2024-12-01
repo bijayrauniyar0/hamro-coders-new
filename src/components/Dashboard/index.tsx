f60723df217f6f87507b0f424b9dac490bf8db5a
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-01 18:26:21 +0545

    feat: add dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { FlexColumn } from '@Components/common/Layouts';
+import TopBox from './TopBox';
+
+const Dashboard = () => {
+  return (
+    <FlexColumn className="w-full gap-2">
+      <TopBox />
+    </FlexColumn>
+  );
+};
+
+export default Dashboard;
