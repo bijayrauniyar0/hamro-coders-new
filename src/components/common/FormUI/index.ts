5453a7dc2e80771a19c25757227c9e11d6363ed2
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-01 18:17:30 +0545

    feat: add FormUI

---
+++ src/components/common/FormUI/index.ts
@@ -0,0 +1,17 @@
+import Label from './Label';
+import Input from './Input';
+import Select from './Select';
+import FormGroup from './FormGroup';
+import FormControl from './FormControl';
+import { DatePicker } from './DatePicker';
+import RadioButton from './Radio';
+
+export {
+  Label,
+  Input,
+  Select,
+  FormControl,
+  FormGroup,
+  DatePicker,
+  RadioButton,
+};
