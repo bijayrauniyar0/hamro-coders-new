802d9d6593f8b8b5cbe6b2e47c6e70924099b627
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-01 18:24:13 +0545

    feat: add modes pahe

---
+++ src/components/Modes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ModesBox from './ModesBox';
+
+const Modes = () => {
+  return (
+    <div className="w-[18rem]">
+      <ModesBox />
+    </div>
+  );
+};
+
+export default Modes;
