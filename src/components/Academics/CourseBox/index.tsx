2256e99941a30361795dc0f83692e535cc2aed4f
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-02 23:43:39 +0545

    feat: update coursebox component

--- src/components/Academics/CourseBox/index.tsx
+++ src/components/Academics/CourseBox/index.tsx
@@ -1,30 +1,31 @@
-import { Select } from '@Components/common/FormUI';
-import { FlexColumn, FlexRow } from '@Components/common/Layouts';
-import { semestersData } from '@Constants/Academics';
-import React, { useState } from 'react';
-import ToolTip from '@Components/radix/ToolTip';
+import { FlexColumn } from '@Components/common/Layouts';
 import SubjectRow from './SubjectRow';
 import { ISubjects } from '@Constants/Types/academics';
 import SubjectBox from './SubjectBox';
-
-interface ICourse {
+import { motion } from 'framer-motion';
+import { cardVariants, containerVariants } from '@Animations/index';
+import { useTypedDispatch } from '@Store/hooks';
+import { setIsModesOpen } from '@Store/actions/common';
+export interface ICourse {
   semester: string;
   semesterNumber: number;
   subjects: ISubjects[];
 }
 
 interface ICourseBoxProps {
-  courseDetails: ICourse[];
-  courseName: string;
+  courseDetails: ISubjects[];
+  selectedStyle: string;
+  img?: string;
+  courseName?: string;
 }
 
-const CourseBox = ({ courseDetails, courseName }: ICourseBoxProps) => {
-  const [selectedOption, setSelectedOption] = useState<string | number>(1);
-  const [filteredList, setFilteredlist] = useState<ISubjects[]>(
-    courseDetails[0].subjects,
-  );
-  const [selectedStyle, setSelectedStyle] = useState<string>('grid');
-
+const CourseBox = ({
+  courseDetails,
+  selectedStyle,
+  // eslint-disable-next-line no-unused-vars
+  courseName,
+}: ICourseBoxProps) => {
+  const dispatch = useTypedDispatch();
   function getStyle() {
     switch (selectedStyle) {
       case 'grid':
@@ -37,12 +38,22 @@ const CourseBox = ({ courseDetails, courseName }: ICourseBoxProps) => {
   }
   function getComponentAccordingToStyle(key: number, semester: ISubjects) {
     switch (selectedStyle) {
-      case 'grid':
-        return <SubjectBox key={key} courseDetails={semester} />;
       case 'list':
-        return <SubjectRow key={key} courseDetails={semester} />;
+        return (
+          <SubjectRow
+            key={key}
+            courseDetails={semester}
+            handlePlay={() => dispatch(setIsModesOpen())}
+          />
+        );
       default:
-        return <SubjectBox key={key} courseDetails={semester} />;
+        return (
+          <SubjectBox
+            key={key}
+            courseDetails={semester}
+            handlePlay={() => dispatch(setIsModesOpen())}
+          />
+        );
     }
   }
 
@@ -86,43 +97,20 @@ const CourseBox = ({ courseDetails, courseName }: ICourseBoxProps) => {
     //   </div>
     // </FlexColumn>
     <FlexColumn className="gap-4">
-      <FlexRow className="items-center justify-between">
-        <p className="text-lg font-bold">{courseName}</p>
-        <FlexRow className="items-center gap-2">
-          <ToolTip
-            name="grid_view"
-            message="Show Grid View"
-            iconClick={() => setSelectedStyle('grid')}
-            className={`${selectedStyle === 'grid' ? 'rounded-sm !bg-secondary-100 p-1' : ''} flex min-w-[2rem] items-center justify-center`}
-          />
-          <ToolTip
-            name="view_list"
-            message="Show List View"
-            iconClick={() => setSelectedStyle('list')}
-            className={`${selectedStyle === 'list' ? 'rounded-sm !bg-secondary-100 p-1' : ''} flex min-w-[2rem] items-center justify-center`}
-          />
-          <Select
-            options={semestersData || []}
-            placeholder="Select"
-            className="!z-0 h-8 !w-[11.25rem] rounded !border border-[#D0D5DD] bg-white"
-            valueKey="value"
-            selectedOption={+selectedOption}
-            onChange={value => {
-              setSelectedOption(value);
-              const filteredSemesterData = courseDetails.filter(
-                semesterX => semesterX.semesterNumber === +value,
-              );
-              setFilteredlist(filteredSemesterData[0].subjects);
-            }}
-          />
-        </FlexRow>
-      </FlexRow>
-      <div className={getStyle()}>
-        {filteredList.map((semester, index) => {
-          // return <SubjectBox key={index} courseDetails={semester} />;
-          return getComponentAccordingToStyle(index, semester);
+      <motion.div
+        className={getStyle()}
+        variants={containerVariants}
+        initial="hidden"
+        animate="show"
+      >
+        {courseDetails?.map((semester, index) => {
+          return (
+            <motion.div variants={cardVariants} key={semester.subjectCode}>
+              {getComponentAccordingToStyle(index, semester)}
+            </motion.div>
+          );
         })}
-      </div>
+      </motion.div>
     </FlexColumn>
   );
 };
