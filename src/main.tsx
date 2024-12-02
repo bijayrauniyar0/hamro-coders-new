d89af4fe231f13ddea3b8e6eedcbc29704348e79
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-02 23:38:27 +0545

    feat: add config for redux

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@Assets/css/index.css';
 import '@Assets/css/tailwind.css';
 import { BrowserRouter } from 'react-router-dom';
+import store from '@Store/index.tsx';
+import { Provider } from 'react-redux';
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </Provider>
   </QueryClientProvider>,
 );
