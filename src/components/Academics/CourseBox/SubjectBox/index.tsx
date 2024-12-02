e361821523d0cf53bc957697d5c0a3c7b0c5b74a
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-02 23:42:45 +0545

    feat: add handleplay and thumbnail props

--- src/components/Academics/CourseBox/SubjectBox/index.tsx
+++ src/components/Academics/CourseBox/SubjectBox/index.tsx
@@ -2,19 +2,23 @@ import { FlexColumn, FlexRow } from '@Components/common/Layouts';
 import { Button } from '@Components/radix/Button';
 import { ISubjectBoxProps } from '@Constants/Types/academics';
 import mcqThumbnail from '@Assets/images/mcq-thumbnail.png';
-import { useNavigate } from 'react-router-dom';
 
-const SubjectBox = ({ courseDetails }: ISubjectBoxProps) => {
-  const navigate = useNavigate();
+const SubjectBox = ({ courseDetails, handlePlay }: ISubjectBoxProps) => {
   return (
     <>
       <FlexColumn className="w-full flex-1 gap-4">
         <div className="h-[12rem] w-full rounded-lg bg-gray-200">
-          <img src={mcqThumbnail} alt="" className="w-full object-cover" />
+          <img
+            src={mcqThumbnail}
+            alt=""
+            className="h-[12rem] w-full rounded-lg object-cover"
+          />
         </div>
         <FlexRow className="items-center justify-between gap-1">
-          <p className="text-base font-medium">{courseDetails.name}</p>
-          <Button onClick={() => navigate('/academics/bca')}>Play</Button>
+          <p className="w-[75%] text-base font-medium leading-5">
+            {courseDetails.name}
+          </p>
+          <Button onClick={handlePlay}>Play</Button>
         </FlexRow>
       </FlexColumn>
     </>
