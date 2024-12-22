1a842a8a9cf1fa1a10834b76e24f46e73de80913
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-12-22 18:28:49 +0545

    feat: add model for users

---
+++ backend/src/models/userModels.ts
@@ -0,0 +1,42 @@
+// src/models/userModel.ts
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database';
+
+// Define the User model
+class User extends Model {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true,
+  },
+);
+
+export default User;
