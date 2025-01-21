f65df27eda46f6ea7c0b66276388fe7ee16541ce
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2025-01-21 20:58:49 +0545

    feat: add authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,23 @@ import 'react-toastify/dist/ReactToastify.css';
 import appRoutes from './routes/appRoutes';
 import generateRoutes from './routes/generateRoutes';
 import Navbar from './components/common/Navbar';
+import { useLocation, useParams } from 'react-router-dom';
 
 function App() {
+  const routesWithoutNavbar = ['/login'];
+  const { pathname } = useLocation();
+  const showNavbar = routesWithoutNavbar.some(
+    route => !pathname.includes(route),
+  );
   return (
     <>
       <div className="fixed top-0 z-[49] w-full bg-white">
-        <Navbar />
+        {showNavbar && <Navbar />}
       </div>
       <div className="absolute right-0 top-4 w-1/4">
         <ToastContainer />
       </div>
-      <div className="min-h-screen w-full bg-[#F4F7FE] px-4 pb-[2rem] pt-[6rem]">
-        <div className="mx-auto w-11/12">
-          {generateRoutes({ routes: appRoutes })}
-        </div>
-      </div>
+      {generateRoutes({ routes: appRoutes })}
     </>
   );
 }
