b362477ed44ddbf8199bbe5361a3939d67962963
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2025-01-21 21:01:38 +0545

    feat: add authentication

---
+++ frontend/src/components/UserAuthentication/Login/index.tsx
@@ -0,0 +1,14 @@
+import { FlexColumn } from '@Components/common/Layouts';
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="flex items-center">
+      <FlexColumn className="gap-2">
+        <p className="text-sm font-medium">Hamro Coders</p>
+      </FlexColumn>
+    </div>
+  );
+};
+
+export default Login;
