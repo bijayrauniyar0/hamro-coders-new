de8cfefd70840460f6f47eac4aa8509070a9b9f5
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-01-28 21:43:45 +0545

    feat: update root serviuce

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -18,9 +18,9 @@ export const authenticated = (apiInstance: AxiosInstance) => {
   const token = localStorage.getItem('token');
   if (!token) return apiInstance;
   if (process.env.NODE_ENV === 'development') {
-    apiInstance.defaults.headers.common.Authorization = `Token ${token}`;
+    apiInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
   } else {
-    apiInstance.defaults.headers.common.Authorization = `Token ${token}`;
+    apiInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
     apiInstance.defaults.withCredentials = false;
   }
   return apiInstance;
