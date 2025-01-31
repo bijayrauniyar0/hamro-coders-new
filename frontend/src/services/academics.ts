d7f0698ae40435bcbcf53fa3d10703e812275241
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-01 00:26:30 +0545

    feat: add services for mcqs

--- frontend/src/services/academics.ts
+++ frontend/src/services/academics.ts
@@ -6,3 +6,15 @@ export const getSubjectsBySemester = async (params: Record<string, any>) => {
     `/api/academics/subjects/${semester}/${course_name}/`,
   );
 };
+
+export const getMcqs = async (params: Record<string, any>) => {
+  const { subject_code } = params;
+  return authenticated(api).get(`/api/mcq/questions/${subject_code}/`);
+};
+
+export const getMcqAnswers = async (paramsX: Record<string, any>) => {
+  const { subject_code, ...params } = paramsX;
+  return authenticated(api).get(`/api/mcq/answers/${subject_code}/`, {
+    params,
+  });
+};
