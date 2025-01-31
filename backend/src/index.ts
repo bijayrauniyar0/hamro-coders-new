21de47e43da2e55d45ab7e09810a22b533601d33
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-01 00:30:28 +0545

    feat: add routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import userRoutes from '@Routes/userRoutes';
 import sequelize from './config/database';
 import cors from 'cors';
+import mcqRouter from '@Routes/mcqsRoutes';
+import academicsRouter from '@Routes/academicRoutes';
 
 const app = express();
 app.use(cors());
@@ -11,6 +13,8 @@ app.use(express.json()); // Middleware to parse JSON requests
 const port = 9000;
 
 app.use('/api/user', userRoutes);
+app.use('/api/mcq', mcqRouter);
+app.use('/api/academics', academicsRouter);
 
 sequelize.authenticate();
 
