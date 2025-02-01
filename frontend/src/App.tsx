822ad03b66d179df4aab17845b9455b5f97f4a32
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-01 22:38:01 +0545

    style: remove fixed positiom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,13 @@ import { useLocation } from 'react-router-dom';
 function App() {
   const routesWithoutNavbar = ['/login', '/signup'];
   const { pathname } = useLocation();
-  const showNavbar = !routesWithoutNavbar.some(route => pathname.includes(route));
-  
+  const showNavbar = !routesWithoutNavbar.some(route =>
+    pathname.includes(route),
+  );
+
   return (
     <>
-      <div className="fixed top-0 z-[49] w-full bg-white">
-        {showNavbar && <Navbar />}
-      </div>
+      {showNavbar && <Navbar />}
       <div className="absolute right-0 top-4 w-1/4">
         <ToastContainer />
       </div>
