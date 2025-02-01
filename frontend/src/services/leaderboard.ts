729127e91d2c15ee2880863b9266ce2f1f0f777d
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-01 21:46:26 +0545

    feat: add services for leaderboard

--- frontend/src/services/leaderboard.ts
+++ frontend/src/services/leaderboard.ts
@@ -5,5 +5,9 @@ export const createLeaderboardEntry = (payload: Record<string, any>) => {
 };
 
 export const getLeaderboard = (params: Record<string, any>) => {
-  return api.get('/api/leaderboard/', { params });
+  return api.get('/api/leaderboard/rank/', { params });
+};
+
+export const getUserStats = () => {
+  return authenticated(api).get('/api/leaderboard/stats/');
 };
