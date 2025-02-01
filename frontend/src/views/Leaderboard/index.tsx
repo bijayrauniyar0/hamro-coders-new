904c8a9caa938d844c1edec09bedbd8a027e1254
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-01 21:46:47 +0545

    feat: add leaderboard page

---
+++ frontend/src/views/Leaderboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Leaderboard = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate qui
+      ipsa laudantium consectetur nemo molestiae? Modi, unde molestias,
+      voluptate vitae tenetur reprehenderit dolorem commodi dicta nemo
+      laudantium dolor nobis atque.
+    </div>
+  );
+};
+
+export default Leaderboard;
