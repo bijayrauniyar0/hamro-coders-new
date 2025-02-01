35f7b570417d94d60d263a3d1014f91720b02ad7
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-01 21:45:30 +0545

    feat: add scores constants

---
+++ frontend/src/constants/Leaderboard/index.ts
@@ -0,0 +1,37 @@
+export const scores = [
+  {
+    user_id: 1,
+    score: 83,
+    User: {
+      name: 'Bijay Rauniyar',
+    },
+  },
+  {
+    user_id: 5,
+    score: 72,
+    User: {
+      name: 'Test 4',
+    },
+  },
+  {
+    user_id: 2,
+    score: 25,
+    User: {
+      name: 'Bijay Kumar',
+    },
+  },
+  {
+    user_id: 4,
+    score: 24,
+    User: {
+      name: 'Test 3',
+    },
+  },
+  {
+    user_id: 3,
+    score: 18,
+    User: {
+      name: 'Test 2',
+    },
+  },
+];
