f29727f13e3d4bc5e7402f03e3292c492886334d
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-05 23:42:20 +0545

    feat: update controller for add score and get score

--- backend/src/controllers/leaderboardController.ts
+++ backend/src/controllers/leaderboardController.ts
@@ -1,122 +1,43 @@
-import UserScores from '@Models/userScoresModels';
-import User from '@Models/userModels';
-import {
-  getEndOfDay,
-  getEndOfMonth,
-  getEndOfWeek,
-  getStartOfDay,
-  getStartOfMonth,
-  getStartOfWeek,
-} from '@Utils/index';
 import { Request, Response } from 'express';
-import { Op } from 'sequelize';
-import Rank from '@Models/rankModel';
-import sequelize from 'src/config/database';
+import UserScores from '@Models/userScoresModels';
+import { getAllUserRanks, getUserRankById } from '@Services/index';
+
+type UserRank = {
+  user_id: number;
+  total_score: number;
+  rank: number;
+};
 
 export const createScoreEntry = async (req: Request, res: Response) => {
-  const transaction = await sequelize.transaction();
-  const startOfDay = getStartOfDay();
-  const endOfDay = getEndOfDay();
-  let newRank;
   try {
     const { subject_code, score, semester } = req.body;
     const user = req.user;
-    const existingRank = await Rank.findOne({
-      where: {
-        user_id: user?.id,
-        created_at: { [Op.between]: [startOfDay, endOfDay] },
+    const currentRank = await getUserRankById(user.id, 'daily');
+    const currentRankWeekly = await getUserRankById(user.id, 'weekly');
+    const currentRankMonthly = await getUserRankById(user.id, 'monthly');
+    await UserScores.create({
+      score,
+      subject_code,
+      user_id: user?.id,
+      semester,
+      previous_rank: {
+        daily: (currentRank as UserRank)?.rank || null,
+        weekly: (currentRankWeekly as UserRank)?.rank || null,
+        monthly: (currentRankMonthly as UserRank)?.rank || null,
       },
     });
-    if (existingRank) {
-      // Update score by adding the new score
-      existingRank.score += score;
-      await existingRank.save({ transaction });
-    } else {
-      // Create a new rank record for today
-      newRank = await Rank.create(
-        { user_id: user?.id, score },
-        { transaction },
-      );
-    }
-    await UserScores.create(
-      {
-        score,
-        subject_code,
-        user_id: user?.id,
-        rank_id: existingRank?.id || newRank?.id,
-        semester,
-      },
-      { transaction },
-    );
-    await transaction.commit();
     res.status(201).json({ message: 'Score added successfully' });
   } catch (error) {
-    transaction.rollback();
     res.status(500).json({ message: 'Internal server error', details: error });
   }
 };
 
-// export const getScores = async (req: Request, res: Response) => {
-//   const { filter_by } = req.query;
-
-//   let startDate = getStartOfDay();
-//   let endDate = getEndOfDay();
-//   if (filter_by === 'week') {
-//     startDate = getStartOfWeek();
-//     endDate = getEndOfWeek();
-//   }
-//   if (filter_by === 'month') {
-//     startDate = getStartOfMonth();
-//     endDate = getEndOfMonth();
-//   }
-//   try {
-//     const scores = await UserScores.findAll({
-//       where: { createdAt: { [Op.between]: [startDate, endDate] } },
-//       order: [['score', 'DESC']],
-//       // limit: 10,
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//       attributes: {
-//         exclude: ['id', 'createdAt', 'subject_code', 'semester'],
-//       },
-//     });
-//     res.status(200).json(scores);
-//   } catch {
-//     res.status(500).json({ message: 'Internal server error' });
-//   }
-// };
-
 export const getRank = async (req: Request, res: Response) => {
   const { filter_by } = req.query;
-
-  let startDate = getStartOfDay();
-  let endDate = getEndOfDay();
-  if (filter_by === 'week') {
-    startDate = getStartOfWeek();
-    endDate = getEndOfWeek();
-  }
-  if (filter_by === 'month') {
-    startDate = getStartOfMonth();
-    endDate = getEndOfMonth();
-  }
   try {
-    const ranks = await Rank.findAll({
-      where: { created_at: { [Op.between]: [startDate, endDate] } },
-      order: [['score', 'DESC']],
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-      attributes: {
-        exclude: ['id', 'created_at', 'updated_at'],
-      },
-    });
+    const ranks = await getAllUserRanks(
+      (filter_by as 'daily' | 'weekly' | 'monthly') || 'daily',
+    );
     res.status(200).json(ranks);
   } catch (error) {
     res.status(500).json({ message: 'Internal server error', details: error });
