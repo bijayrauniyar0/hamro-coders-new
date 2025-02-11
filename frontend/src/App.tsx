060f03e556ecebd2a34c72eb2fea1cb8a6b7aed9
bijayrauniyar0 <rauniyarbijay0@gmail.com>
2025-02-11 22:30:42 +0545

    feat: add check login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,39 @@ import appRoutes from './routes/appRoutes';
 import generateRoutes from './routes/generateRoutes';
 import Navbar from './components/common/Navbar';
 import { useLocation } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { checkLogin, getUserById } from '@Services/common';
+import { useEffect } from 'react';
+import { useTypedDispatch } from '@Store/hooks';
+import { setUserProfile } from '@Store/actions/common';
 
 function App() {
-  const routesWithoutNavbar = ['/login', '/signup'];
   const { pathname } = useLocation();
+  const dispatch = useTypedDispatch();
+
+  const routesWithoutNavbar = ['/login', '/signup'];
   const showNavbar = !routesWithoutNavbar.some(route =>
     pathname.includes(route),
   );
 
+  const { isSuccess: isUserLoggedIn, data: userId } = useQuery({
+    queryKey: ['user-profile'],
+    queryFn: () => checkLogin(),
+    select: ({ data }) => data?.id,
+  });
+
+  const { isSuccess: userProfileIsFetched, data: userProfile } = useQuery({
+    queryKey: ['user-profile'],
+    queryFn: () => getUserById(userId),
+    select: ({ data }) => data,
+    enabled: isUserLoggedIn && !!userId,
+  });
+
+  useEffect(() => {
+    if (userProfileIsFetched) {
+      dispatch(setUserProfile(userProfile));
+    }
+  }, [userProfileIsFetched, userProfile]);
   return (
     <>
       {showNavbar && <Navbar />}
