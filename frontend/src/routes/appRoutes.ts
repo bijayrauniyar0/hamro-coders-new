24ceaf352379cf90416b6d433c83b586bd847296
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-04-03 20:45:01 +0545

    refactor: remove mcq routes

--- frontend/src/routes/appRoutes.ts
+++ frontend/src/routes/appRoutes.ts
@@ -2,9 +2,7 @@ import CoursesPage from '@Views/Courses';
 import { IRoute } from './type';
 import DashboardPage from '@Views/Dashboard';
 // import ModesPage from '@Views/Modes';
-import MCQBox from '@Components/Courses/MCQSection';
 import AuthenticationComponent from '@Components/UserAuthentication';
-import MCQ from '@Components/Courses/MCQSection';
 import LeaderboardPage from '@Views/LeaderboardPage';
 
 const appRoutes: IRoute[] = [
@@ -28,16 +26,6 @@ const appRoutes: IRoute[] = [
     path: '/courses/*',
     component: CoursesPage,
     authenticated: true,
-  },
-  {
-    name: 'Coding',
-    path: '/coding',
-    component: MCQBox,
-  },
-  {
-    name: 'MCQ',
-    path: '/mcq',
-    component: MCQ,
   },
   {
     name: 'Leaderboard',
