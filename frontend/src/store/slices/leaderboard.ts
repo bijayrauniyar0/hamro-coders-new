50439867d8615560f0ddd2d584d50301daa7ec75
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-04-04 00:05:13 +0545

    feat: add redux slice for leaderboard

---
+++ frontend/src/store/slices/leaderboard.ts
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type LeaderboardState = {
+  filters: {
+    course_id: number;
+    subject_id: number[];
+    filter_by: 'daily' | 'weekly' | 'monthly';
+  };
+  isFiltersOpen: boolean;
+};
+
+const initialState: LeaderboardState = {
+  filters: {
+    course_id: 0,
+    subject_id: [],
+    filter_by: 'daily',
+  },
+  isFiltersOpen: false,
+};
+
+const setFilters = (
+  state: LeaderboardState,
+  action: { payload: Partial<LeaderboardState['filters']> },
+) => {
+  const { payload } = action;
+  state.filters = { ...state.filters, ...payload };
+};
+
+const resetFilters = (state: LeaderboardState) => {
+  state.filters = initialState.filters;
+};
+
+const setIsFiltersOpen = (
+  state: LeaderboardState,
+  action: { payload: boolean },
+) => {
+  const { payload } = action;
+  state.isFiltersOpen = payload;
+};
+
+const leaderboardSlice = createSlice({
+  name: 'leaderboard',
+  initialState,
+  reducers: {
+    setFilters,
+    resetFilters,
+    setIsFiltersOpen,
+  },
+});
+
+export { leaderboardSlice };
+export default leaderboardSlice.reducer;
