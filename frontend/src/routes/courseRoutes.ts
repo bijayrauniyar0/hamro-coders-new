5a451c0a6764544c92294937b45a8e1585bc90aa
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-04-03 20:45:17 +0545

    feat: add routes for mcq

--- frontend/src/routes/courseRoutes.ts
+++ frontend/src/routes/courseRoutes.ts
@@ -1,6 +1,7 @@
 import { IRoute } from './type';
 import Subjects from '@Components/Courses/Subjects';
 import Courses from '@Components/Courses';
+import MCQBox from '@Components/Courses/MCQSection';
 
 const courseRoutes: IRoute[] = [
   {
@@ -13,6 +14,12 @@ const courseRoutes: IRoute[] = [
     name: 'Subjects',
     path: '/:course_id',
     component: Subjects,
+    authenticated: true,
+  },
+  {
+    name: 'MCQ',
+    path: '/:course_id/mcq/*',
+    component: MCQBox,
     authenticated: true,
   },
 ];
