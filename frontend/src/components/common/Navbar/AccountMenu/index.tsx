d555eff11df2e01541d53175697f5b16db898a49
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-04-17 20:39:18 +0545

    feat: update account menu

--- frontend/src/components/common/Navbar/AccountMenu/index.tsx
+++ frontend/src/components/common/Navbar/AccountMenu/index.tsx
@@ -6,20 +6,14 @@ import {
   DropdownMenuTrigger,
 } from '@Components/radix/DropDownMenu';
 import Icon from '@Components/common/Icon';
-import { FlexColumn, FlexRow } from '@Components/common/Layouts';
-import Flex from '@Components/common/Layouts/Flex';
-import { Button } from 'react-day-picker';
+import { FlexRow } from '@Components/common/Layouts';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-// import { useTypedSelector } from '@Store/hooks';
-// import { useDispatch } from 'react-redux';
+import { DropdownMenuItem } from '@radix-ui/react-dropdown-menu';
 
 const AccountMenu = () => {
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
-  // const [accountSettingsOpen, setAccountSettingsOpen] = useState(false);
   const [accountDropdownOpen, setAccountDropdownOpen] = useState(false);
-  const hasProfilePicture = false;
   const userProfile = {
     username: 'Loop Verse',
     group_name: 'Admin',
@@ -49,54 +43,33 @@ const AccountMenu = () => {
         </DropdownMenuTrigger>
         <DropdownMenuContent
           align="end"
-          className="w-[15rem] py-2 shadow-[0px_2px_20px_4px_rgba(0,0,0,0.12)] sm:w-[17.5rem]"
+          className="w-[15rem] !p-0 shadow-[0px_2px_20px_4px_rgba(0,0,0,0.12)] sm:w-[17.5rem]"
         >
-          <FlexRow className="gap-3 border-b-[1px] border-[#D7D7D7] px-3 py-4">
-            <div>
-              {hasProfilePicture ? (
-                <div className="h-[2.8rem] w-[2.8rem] items-center justify-center rounded-full sm:h-[3.5rem] sm:w-[3.5rem]">
-                  <img src={avatarLogo} alt="profile" className="w-full" />
-                </div>
-              ) : (
-                <Flex className="h-[2.8rem] w-[2.8rem] items-center justify-center rounded-full bg-[#417EC9] sm:h-[3.5rem] sm:w-[3.5rem]">
-                  <p className="text-[1.5rem] uppercase text-white sm:text-[1.727rem]">
-                    {/* {getInitialsFromString(userProfile?.username || '')} */}
-                    LV
-                  </p>
-                </Flex>
-              )}
+          <FlexRow className="items-center gap-3 border-b-[1px] border-[#D7D7D7] px-3 py-2">
+            <div className="h-[2.2rem] w-[2.2rem] items-center justify-center rounded-full sm:h-[2.5rem] sm:w-[2.5rem]">
+              <img src={avatarLogo} alt="profile" className="w-full" />
             </div>
-            <div className="line-clamp-3 text-[0.875rem]">
-              <p className="line-clamp-1 text-[1.063rem] font-bold uppercase text-[#475467]">
-                {userProfile?.username || ''}
-              </p>
-              <p className="text-[0.875rem] text-[#667085]">
-                {userProfile?.group_name || ''}
-              </p>
-            </div>
+            <p className="line-clamp-1 text-sm font-bold uppercase text-[#475467] sm:text-base">
+              {userProfile?.username || ''}
+            </p>
           </FlexRow>
-          <FlexColumn className="mt-3">
-            <Button
-              onClick={() => {
-                setAccountDropdownOpen(false);
-                // setAccountSettingsOpen(true);
-              }}
-            >
-              <FlexRow className="items-center gap-2 p-3 hover:bg-[#F5FAFF]">
-                <Icon name="settings" className="text-[#475467]" />
-                <p className="pb-1 text-[0.938rem] text-[#475467]">
-                  Account settings
-                </p>
-              </FlexRow>
-            </Button>
-          </FlexColumn>
-          <FlexRow
-            className="cursor-pointer items-center gap-2 p-3 hover:bg-[#F5FAFF]"
+          <DropdownMenuItem
+            className="flex cursor-pointer items-center gap-2 p-3 hover:bg-primary-100"
+            onClick={() => {
+              setAccountDropdownOpen(false);
+              // setAccountSettingsOpen(true);
+            }}
+          >
+            <Icon name="settings" className="text-[#475467]" />
+            <p className="pb-1 text-md text-[#475467]">Account settings</p>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex cursor-pointer items-center gap-2 p-3 hover:bg-primary-100"
             onClick={handleLogout}
           >
             <Icon name="logout" className="text-[#475467]" />
-            <p className="pb-1 text-[0.938rem] text-[#475467]">Logout</p>
-          </FlexRow>
+            <p className="pb-1 text-md text-[#475467]">Logout</p>
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </>
