7ae06e311ed48b245509ef52115d982cd48fc6ab
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-04-22 00:43:26 +0545

    feat: add sequalize attribute types

---
+++ backend/src/constants/Types/index.ts
@@ -0,0 +1,10 @@
+import { IncludeOptions, WhereOptions } from 'sequelize';
+
+export interface SequelizeAttributes {
+  where: WhereOptions; // Define the where conditions, based on the columns in the table
+  raw: boolean; // Whether to return raw data (no sequelize instance)
+  attributes: string[] | { exclude: string[] }; // Attributes to select or exclude
+  include: IncludeOptions[]; // Include options for related models (such as Subject)
+  order: [string, 'ASC' | 'DESC'][]; // Order by clause
+  limit: number; // Limit the number of results
+}
