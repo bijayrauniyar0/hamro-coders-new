4e7a1f12ed6edeb4ed132bbd46909af592c75901
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-04-22 00:53:13 +0545

    feat: add services for analytics

---
+++ frontend/src/services/userStats.ts
@@ -0,0 +1,17 @@
+import { api, authenticated } from '.';
+
+export const getUserStats = async (params: Record<string, any>) => {
+  return authenticated(api).get('/api/analytics/stats/', { params });
+};
+
+export const getRecentSessions = async () => {
+  return authenticated(api).get('/api/analytics/recent-sessions/');
+};
+
+export const getPerformanceDetails = async (params: Record<string, any>) => {
+  return authenticated(api).get('/api/analytics/performance-details/', { params });
+};
+
+export const getPerformanceTrend = async (params: Record<string, any>) => {
+  return authenticated(api).get('/api/analytics/performance-trend/', { params });
+};
