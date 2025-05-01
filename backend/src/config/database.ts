8f3de3c6c741efd8fcfe8a437710bc459557e68a
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-05-02 00:58:40 +0545

    feat: upddate connections string

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,16 +5,10 @@ import pg from 'pg';
 
 dotenv.config();
 // Ensure 'port' is correctly converted to a number
-const sequelize = new Sequelize({
-  host: process.env.DB_HOST || 'hamrocoders-sql-db',
-  port: parseInt(process.env.DB_PORT || '5432', 10), // Convert to number here
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const sequelize = new Sequelize(process.env.DB_CONNECTION || '', {
   dialect: 'postgres',
   timezone: '+05:45',
   dialectModule: pg,
-  ssl: true,
 });
 
 export default sequelize;
