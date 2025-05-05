bf6125880a02550e24f74dbedb16cd64292a48d7
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-05-06 00:15:16 +0545

    feat: error handling

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,6 +5,9 @@ import pg from 'pg';
 
 dotenv.config();
 // Ensure 'port' is correctly converted to a number
+if (!process.env.DB_CONNECTION) {
+  throw new Error("Environment variable DB_CONNECTION is not set");
+}
 const sequelize = new Sequelize(process.env.DB_CONNECTION || '', {
   dialect: 'postgres',
   timezone: '+05:45',
