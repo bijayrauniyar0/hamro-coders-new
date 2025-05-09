80be0ca08958886c2ee272ce24595ff8c927eb75
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-05-09 21:07:28 +0545

    feat: error handle

--- backend/src/config/drive.ts
+++ backend/src/config/drive.ts
@@ -3,11 +3,15 @@ import { refreshAccessToken } from '../services/googleTokens'; // Assuming you h
 import oauth2Client from './google'; // The same OAuth2 client you're already using
 
 async function setCredentials() {
-  const accessToken = await refreshAccessToken();
-  if (!accessToken) {
-    throw new Error('No access token found');
+  try {
+    const accessToken = await refreshAccessToken();
+    if (!accessToken) {
+      throw new Error('No access token found');
+    }
+    oauth2Client.setCredentials({ access_token: accessToken });
+  } catch {
+    throw new Error('Failed to set credentials');
   }
-  oauth2Client.setCredentials({ access_token: accessToken });
 }
 
 const getDriveInstance = async () => {
