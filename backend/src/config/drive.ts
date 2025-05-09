1a26323d5adb86af61c4565fe912310c22845659
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-05-09 13:18:04 +0545

    feat: add drive config

---
+++ backend/src/config/drive.ts
@@ -0,0 +1,18 @@
+import { google } from 'googleapis';
+import { refreshAccessToken } from '../services/googleTokens'; // Assuming you have this service for token refresh
+import oauth2Client from './google'; // The same OAuth2 client you're already using
+
+async function setCredentials() {
+  const accessToken = await refreshAccessToken();
+  if (!accessToken) {
+    throw new Error('No access token found');
+  }
+  oauth2Client.setCredentials({ access_token: accessToken });
+}
+
+const getDriveInstance = async () => {
+  await setCredentials(); // Make sure we have fresh credentials
+  return google.drive({ version: 'v3', auth: oauth2Client });
+};
+
+export { getDriveInstance };
