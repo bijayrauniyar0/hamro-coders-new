a100e49ded9eba9a371d10a7726ae42a40b0480b
rauniyarbijay0 <rauniyarbijay0@gmail.com>
2025-05-13 23:58:27 +0545

    feat: update image upload

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -23,7 +23,9 @@ export const getUserProfile = async (
     const { user } = req;
     const userData = await User.findOne({
       where: { id: user.id },
-      attributes: { exclude: ['password', 'created_at', 'updated_at'] },
+      attributes: {
+        exclude: ['password', 'created_at', 'updated_at', 'blob_name'],
+      },
     });
     res.status(200).json(userData);
   } catch (error) {
@@ -34,24 +36,30 @@ export const getUserProfile = async (
 // Update user by ID
 export const updateUser = async (req: Request, res: Response): Promise<any> => {
   try {
-    const { password, old_password, ...restValues } = req.body;
-    const { file } = req;
-    let fileResponse;
-    const azureService = new AzureBlobService();
-
-    if (file) {
-      const filePath = file.path;
-      const blobName = `users/${Date.now()}-${file.originalname}`;
-      const url = await azureService.uploadProfilePic(filePath, blobName);
-      fileResponse = {
-        url,
-        blobName,
-      };
-    }
-    const payload: Partial<User> = { ...restValues };
+    // Validate user existence
     const user: User | null = await User.findByPk(req.user.id);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
+    }
+    const { password, old_password, ...restValues } = req.body;
+    const { file } = req;
+    let fileResponse;
+
+    // If there's a file, try to upload it to Azure
+    if (file) {
+      const azureService = new AzureBlobService();
+      const filePath = file.path;
+      const blobName = `users/${Date.now()}-${file.originalname}`;
+
+      try {
+        const url = await azureService.uploadProfilePic(filePath, blobName);
+        fileResponse = { url, blobName };
+      } catch (error) {
+        return res.status(500).json({
+          message: 'Error uploading file to Azure Blob Storage',
+          error,
+        });
+      }
     }
     if ((old_password || password) && (!old_password || !password)) {
       return res.status(400).json({
@@ -59,22 +67,30 @@ export const updateUser = async (req: Request, res: Response): Promise<any> => {
           'Both old password and new password are required to change the password',
       });
     }
+    // If password is provided, check if the old password is valid and then hash the new password
     if (old_password && password) {
       const isPasswordValid = await bcrypt.compare(old_password, user.password);
       if (!isPasswordValid) {
         return res.status(401).json({ message: 'Invalid old password' });
       }
-      user.password = bcrypt.hashSync(password, 10);
+      user.password = bcrypt.hashSync(password, 10); // Hash new password
     }
+
+    // If file was uploaded, update the user avatar URL and blob name
     if (fileResponse) {
-      payload.avatar = fileResponse.url;
-      payload.blob_name = fileResponse.blobName;
+      restValues.avatar = fileResponse.url;
+      restValues.blob_name = fileResponse.blobName;
     }
-    Object.assign(user, payload);
-    await user.save();
-    res.status(200).json({ message: 'User updated successfully' });
+
+    // Only update if there are changes to save
+    if (Object.keys(restValues).length > 0) {
+      Object.assign(user, restValues); // Assign the rest of the values to the user object
+      await user.save(); // Save updated user
+    }
+
+    return res.status(200).json({ message: 'User updated successfully' });
   } catch (error) {
-    res.status(500).json({ message: 'Error updating user', error });
+    return res.status(500).json({ message: 'Error updating user', error });
   }
 };
 
